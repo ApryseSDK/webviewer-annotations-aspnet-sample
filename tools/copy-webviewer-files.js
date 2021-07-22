673c4c560d1b43c0316b063156af351ca50e3fbc
S0ulDrag0n <SD.S0ulDrag0n@gmail.com>
2021-07-21 21:04:02 -0700

    Fixed script to copy over WV file

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra');
 const copyFiles = async () => {
   try {
     await fs.copy('./node_modules/@pdftron/webviewer/public', './server/webviewer-annotations-asp-net-sample/Scripts/webviewer/lib');
+    await fs.copy('./node_modules/@pdftron/webviewer/webviewer.min.js', './server/webviewer-annotations-asp-net-sample/Scripts/webviewer/lib/webviewer.min.js');
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
