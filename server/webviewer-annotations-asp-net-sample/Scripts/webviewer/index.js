3b5c218b648c166dc61368a7833af7ae7098c2fb
Andy <sd.s0uldrag0n@gmail.com>
2023-11-10 12:52:39 -0800

    Updated sample to latest framework (#5)

--- server/webviewer-annotations-asp-net-sample/Scripts/webviewer/index.js
+++ server/webviewer-annotations-asp-net-sample/Scripts/webviewer/index.js
@@ -6,10 +6,10 @@ WebViewer({
   initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo.pdf',
   documentXFDFRetriever: () => loadXfdfString(DOCUMENT_ID)
 }, viewerElement).then(instance => {
-  var annotManager = instance.docViewer.getAnnotationManager();
+  var annotManager = instance.Core.documentViewer.getAnnotationManager();
 
   // Add a save button on header
-  instance.setHeaderItems(function(header) {
+  instance.UI.setHeaderItems(function(header) {
     header.push({
       type: 'actionButton',
       img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
@@ -50,7 +50,7 @@ var loadXfdfString = function(id) {
     fetch(`/api/annotation?id=${id}`, {
       method: 'GET'
     }).then(function(res) {
-      if (res.status === 200) {
+      if (res.status === 200 || res.status === 204) {
         res.text().then(function(xfdfString) {
           resolve(xfdfString);
         });
